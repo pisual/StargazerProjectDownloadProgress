9d8d8fa6cbf47e83c87287d36ca257f8e7be3d46
FelixSion <pisual@163.com>
2017-08-21 22:04:02 +0800

    进度条书写

--- src/com/stargazerproject/download/ProgressDisplay.java
+++ src/com/stargazerproject/download/ProgressDisplay.java
@@ -1,5 +1,74 @@
 package com.stargazerproject.download;
 
-public class ProgressDisplay {
+import java.util.concurrent.TimeUnit;
 
+public class ProgressDisplay {
+	
+	/**
+	 * 标准输出显示
+	 *[▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇][100%]
+	 *[                                                                                   ]
+	 *[▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇        ][100%]
+	 *[▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆][100%]
+	 *50个 ▇
+	 *每增加百分比2，增加一个格子
+	 *
+	 * **/
+	
+	private Integer point = 0;
+	private Integer[] progress = new Integer[50];
+	
+	public ProgressDisplay() {
+		for (Integer p : progress) {
+			p = 0;
+		}
+	}
+	
+	public void standardProgressDisplay(ProgressCalculate progressCalculate){
+		while(true){
+			int schedule = progressCalculate.ProgressPercentage();
+			if((schedule - point) > 0){
+				increaseProgress(schedule);
+				point = schedule;
+			}
+			
+			if(schedule == 100){
+				break;
+			}
+			
+			displayConvert();
+			
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private void increaseProgress(int schedule){
+		for(int i = point; i<schedule; i++){
+			progress[i] = 1;
+		}
+	}
+	
+	
+	private void displayConvert(){
+		StringBuffer progressDispkay = new StringBuffer();
+		progressDispkay.append("[");
+		
+		for (Integer p : progress) {
+			if(p == 0){
+				progressDispkay.append(" ");
+			}
+			else{
+				progressDispkay.append("▇▇");
+			}
+		}
+		
+		progressDispkay.append("][ ");
+		progressDispkay.append(point + "% ]");
+	}
+	
 }
